81beb36f83a38262e46943d6c4188b8f349c37bd
Turbojcat <turbojcat@gmail.com>
2024-05-21 04:07:13 +0200

    modified:   src/events/messages/messagesCreate.js

--- src/events/messages/messagesCreate.js
+++ src/events/messages/messagesCreate.js
@@ -3,7 +3,7 @@ const { PREFIX } = require('../../config');
 module.exports = {
   name: 'messageCreate',
   async execute(message) {
-    if (message.author.bot || !message.content.startsWith(PREFIX)) return;
+    if (!message.content.startsWith(PREFIX) || message.author.bot) return;
 
     const args = message.content.slice(PREFIX.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
